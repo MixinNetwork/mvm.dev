416c29408c325dc810bf9799571b6d041e7d10de
hundredark <hundredark@gmail.com>
2025-04-18 17:13:10 +0800

    init project

---
+++ .vuepress/config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vuepress/config";
+
+export default defineConfig({
+  themeConfig: {
+    repo: "vuejs/vuepress",
+    editLinks: true,
+    docsDir: "packages/docs/docs"
+  }
+});
---
+++ docs/README.md
@@ -0,0 +1,5 @@
+# 介绍
+
+这是 Mixin Safe Computer 的开发者教程
+
+#### [开源代码](https://github.com/MixinNetwork/trusted-group/tree/master/mvm)
